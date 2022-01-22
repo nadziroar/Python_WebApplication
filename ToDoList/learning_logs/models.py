ff6fec60c817f1e1ed974adb8060794466219063
nadziroar <nadzirahzaid144@gmail.com>
2022-01-22 11:22:20 +0800

    Update the Category, Tasks model

--- ToDoList/learning_logs/models.py
+++ ToDoList/learning_logs/models.py
@@ -4,16 +4,16 @@ from django.db import models
 
 # Create your models here.
 
-class Category  (models.Model):
+class Category (models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
 
-class Tasks (models.Model):
+class Tasks(models.Model):
     """Create a task """
-    category = models.ForeignKey(Category , on_delete=CASCADE)
+    category = models.ForeignKey(Category , on_delete= models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add= True)
 
