dc552eebb96d937340de78784e54c0151993f627
nadziroar <nadzirahzaid144@gmail.com>
2022-01-22 11:13:34 +0800

    Create Category, Task models

--- ToDoList/learning_logs/models.py
+++ ToDoList/learning_logs/models.py
@@ -1,3 +1,5 @@
+from pydoc import text
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
@@ -6,6 +8,17 @@ class Category  (models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-def __str__(self) : 
-    """Return a string representation of the model"""
-    return self.text
+    def __str__(self):
+        return self.text
+
+class Tasks (models.Model):
+    """Create a task """
+    category = models.ForeignKey(Category , on_delete=CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add= True)
+
+    class Meta:
+        verbose_name_plural = 'Tasks'
+
+    def __str__(self) :
+        return f"{self.text[:50]}"
