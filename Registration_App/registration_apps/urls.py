4a42eac61ee60309136f4b5d060a7ae5eb44e461
nadziroar <nadzirahzaid144@gmail.com>
2022-02-01 20:51:10 +0800

    Add URL PAGE

---
+++ Registration_App/registration_apps/urls.py
@@ -0,0 +1,11 @@
+"""Defines URL patterns for registration_apps"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'registration_apps'
+urlpatterns = [
+    #Home Page : 
+    path('' , views.index , name = 'index'),
+]
