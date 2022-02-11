49b30476541f0b960ebe3173ce3c97772673be12
nadziroar <nadzirahzaid144@gmail.com>
2022-02-11 14:34:15 +0800

    Update

--- Registration_App/registration_apps/admin.py
+++ Registration_App/registration_apps/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import StudentClass , StudentInfo
+
 admin.site.register(StudentClass)
 admin.site.register(StudentInfo)
--- Registration_App/registration_apps/views.py
+++ Registration_App/registration_apps/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from .models import StudentInfo
+from .models import StudentInfo, StudentClass
 
 # Create your views here.
 def index(request):
     """The Home Page for Registration Page"""
     return render(request, 'registration_apps/index.html')
 
-def students_information(request):
+def students_class(request):
+    """Show all classes """
+    student_class = StudentClass.objects.order_by('current_capacity')
+
+def students_info(request):
     """Show all students"""
     students_info = StudentInfo.objects.order_by('id')
-    context = {'students' : students_info}
+    context = {'student_info' : students_info}
     return render (request , 'registration_apps/students.html', context)
