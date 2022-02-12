0870bcf281bb21680ef9ff70d87a58f175f70b86
nadziroar <nadzirahzaid144@gmail.com>
2022-02-12 14:26:19 +0800

    Created StudentClass & StudentInfo models

--- Student_Management_System/learning_logs/models.py
+++ Student_Management_System/learning_logs/models.py
@@ -1,18 +1,31 @@
-from enum import unique
-from pydoc import classname
+
+from datetime import timezone
+from xml.etree.ElementTree import tostring
 from django.db import models
 
 # Create your models here.
 
 class StudentClass(models.Model):
     #Class for student classes
-    classID = models.Index(unique = True)
-    className  = models.CharField(max_length= 50)
-    classMaxCapacity = models.IntegerField
-    classCurrentCapacity = models.IntegerField
+    className  = models.CharField(unique = True , max_length= 50)
+    classMaxCapacity = models.IntegerField()
+    classCurrentCapacity = models.IntegerField()
     createdDate = models.DateTimeField(auto_now_add= True)
     updateDate = models.DateTimeField(auto_now_add= True)
     
+    
     #This is to return classname with current capacity
     def __str__(self):
-        return self.classID + "." +self.className + "Current Capacity: " + self.classCurrentCapacity
+        return self.className + ' Current Capacity : ' + str(self.classCurrentCapacity) + '/' + str(self.classMaxCapacity)
+    
+ 
+    
+class Student (models.Model):
+    #CLASS MODEL FOR STUDENT_INFORMATION
+    studentID = models.IntegerField(primary_key= True)
+    studentFirstName = models.CharField(max_length= 300)
+    studentLastName = models.CharField (max_length= 300)
+    studentClass = models.ForeignKey (StudentClass , on_delete= models.CASCADE)
+    studentRegisterDate = models.DateTimeField (auto_now_add= True)
+    studentEmail = models.CharField(unique = True , max_length= 300)
+    studentStatus = ()
